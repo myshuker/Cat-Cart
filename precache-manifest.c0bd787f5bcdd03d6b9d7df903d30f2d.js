3acf067368cd8cf92bb2d6b067e84711c71a6172
myshuker <myshuker@gmai.com>
2020-08-17 01:27:27 +0200

    Updates

---
+++ precache-manifest.c0bd787f5bcdd03d6b9d7df903d30f2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78e71646b487cadec4f9305d3d17c84",
+    "url": "/Cat-Cart/index.html"
+  },
+  {
+    "revision": "3d564ca2a3895cbe0d8f",
+    "url": "/Cat-Cart/static/css/main.89b790b6.chunk.css"
+  },
+  {
+    "revision": "2b2fde9326a33b27b282",
+    "url": "/Cat-Cart/static/js/2.ccfed530.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Cat-Cart/static/js/2.ccfed530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d564ca2a3895cbe0d8f",
+    "url": "/Cat-Cart/static/js/main.d9a8099c.chunk.js"
+  },
+  {
+    "revision": "77c64c970694888f3e6f",
+    "url": "/Cat-Cart/static/js/runtime-main.7e3c868b.js"
+  }
+]);
